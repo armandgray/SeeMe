86f2cd697d86c3c6412709721dc1696ea52b33c9
Armand Gray <armandgray@gmail.com>
2017-03-08 10:14:47 -0800

    added username and secret to dummy Users

--- main.go
+++ main.go
@@ -9,12 +9,16 @@ import (
 type User struct {
   FirstName string
   LastName string
+  Username string
+  Secret []byte
 }
 
 func main()  {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    user1 := User{"Armand", "Gray"}
-    user2 := User{"Daniela", "Meza"}
+    var s []byte
+    s = make([]byte, 5, 5)
+    user1 := User{"Armand", "Gray", "email", s}
+    user2 := User{"Daniela", "Meza", "username", s}
     slcUser := []User{user1, user2}
       
     js, err := json.Marshal(slcUser)
