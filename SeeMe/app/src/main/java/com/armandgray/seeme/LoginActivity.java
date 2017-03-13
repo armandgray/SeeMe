3b5c4112802a87204a4a88214b96514a11e754c3
Armand Gray <armandgray@gmail.com>
2017-03-13 12:25:12 -0700

    created Broadcast Reciever for login

--- SeeMe/app/src/main/java/com/armandgray/seeme/LoginActivity.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.armandgray.seeme;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.armandgray.seeme.models.User;
 import com.armandgray.seeme.services.HttpService;
 
 import static com.armandgray.seeme.MainActivity.API_URI;
@@ -16,20 +20,39 @@ import static com.armandgray.seeme.MainActivity.API_URI;
 public class LoginActivity extends AppCompatActivity {
 
     public static final String LOGIN_URI = API_URI + "/login?login=Log+In";
+    public static final String LOGIN_PAYLOAD = "LOGIN_PAYLOAD";
+    private EditText etUsername;
+    private EditText etPassword;
+
+    private BroadcastReceiver httpBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("BroadcastReceiver: ", "http Broadcast Received");
+            User[] userList = (User[]) intent.getParcelableArrayExtra(HttpService.HTTP_SERVICE_PAYLOAD);
+            if (userList[0] == null) {
+                Toast.makeText(LoginActivity.this,
+                        "User " + etUsername.getText().toString() + " Not Found!",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Intent loginIntent = new Intent(context, MainActivity.class);
+                loginIntent.putExtra(LOGIN_PAYLOAD, userList[0]);
+                startActivity(loginIntent);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
 
         Button btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(LoginActivity.this, "Authenticating...", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(LoginActivity.this, HttpService.class);
                 intent.setData(Uri.parse(LOGIN_URI
                         + "&username=" + etUsername.getText().toString()
