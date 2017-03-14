c5366439b787add438d3ef5621e4310b41ccbbb5
Armand Gray <armandgray@gmail.com>
2017-03-13 20:10:23 -0700

    added onClick listeners to buttons

--- SeeMe/app/src/main/java/com/armandgray/seeme/RegisterActivity.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/RegisterActivity.java
@@ -1,15 +1,51 @@
 package com.armandgray.seeme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private RegisterController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
+
+        controller = null;
+
+        Button btnCreateAccount = (Button) findViewById(R.id.btnCreateAccount);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                controller.onAccountSubmit();
+            }
+        });
+
+        ImageView ivBackButton = (ImageView) findViewById(R.id.ivBackButton);
+        ivBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        TextView tvAlreadyHaveAccount = (TextView) findViewById(R.id.tvAlreadyHaveAccount);
+        tvAlreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    public interface RegisterController {
+        void onAccountSubmit();
     }
 }
