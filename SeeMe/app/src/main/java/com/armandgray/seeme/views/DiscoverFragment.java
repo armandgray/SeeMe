e335b0d5ae24581cd562060ef86407a8d69280bb
Armand Gray <armandgray@gmail.com>
2017-03-14 22:37:37 -0700

    added BroadcastReceiver to Discovery and reg/unreg

--- SeeMe/app/src/main/java/com/armandgray/seeme/views/DiscoverFragment.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/views/DiscoverFragment.java
@@ -1,19 +1,48 @@
 package com.armandgray.seeme.views;
 
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.armandgray.seeme.R;
+import com.armandgray.seeme.models.User;
+import com.armandgray.seeme.services.HttpService;
+import com.armandgray.seeme.utils.UserRVAdapter;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DiscoverFragment extends Fragment {
 
+    private RecyclerView rvUsers;
+
+    private BroadcastReceiver httpBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("BroadcastReceiver: ", "http Broadcast Received");
+            User[] userList =
+                    (User[]) intent.getParcelableArrayExtra(HttpService.HTTP_SERVICE_PAYLOAD);
+            if (userList != null) {
+                setupRvUsers(Arrays.asList(userList));
+            } else {
+                Log.i("USER_LIST", "LIST IS NULL");
+            }
+        }
+    };
 
     public DiscoverFragment() {
         // Required empty public constructor
@@ -23,7 +52,36 @@ public class DiscoverFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_discover, container, false);
+
+        rvUsers = (RecyclerView) rootView.findViewById(R.id.rvUsers);
+
+        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(
+                getActivity().getApplicationContext());
+        broadcastManager.registerReceiver(httpBroadcastReceiver,
+                new IntentFilter(HttpService.HTTP_SERVICE_MESSAGE));
+
+        return rootView;
     }
 
+    private void setupRvUsers(List<User> list) {
+        rvUsers.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        rvUsers.setAdapter(new UserRVAdapter(getActivity(), list));
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(getActivity().getApplicationContext())
+                .registerReceiver(httpBroadcastReceiver,
+                        new IntentFilter(HttpService.HTTP_SERVICE_MESSAGE));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(getActivity().getApplicationContext())
+                .unregisterReceiver(httpBroadcastReceiver);
+    }
 }
