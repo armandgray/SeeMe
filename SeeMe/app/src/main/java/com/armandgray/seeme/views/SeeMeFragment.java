1d9d8288081d4b39af698c5d2bf4064fcd01519d
Armand Gray <armandgray@gmail.com>
2017-03-14 22:10:01 -0700

    added getWifiNetworkId() and ic_wifi as flag

--- SeeMe/app/src/main/java/com/armandgray/seeme/views/SeeMeFragment.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/views/SeeMeFragment.java
@@ -1,11 +1,19 @@
 package com.armandgray.seeme.views;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.armandgray.seeme.R;
 
@@ -14,6 +22,11 @@ import com.armandgray.seeme.R;
  */
 public class SeeMeFragment extends Fragment {
 
+
+    private boolean isWifiConnected;
+    private String ssid;
+    private String networkId;
+    private ImageView ivWifi;
 
     public SeeMeFragment() {
     }
@@ -24,9 +37,41 @@ public class SeeMeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_see_me, container, false);
 
+        ivWifi = (ImageView) rootView.findViewById(R.id.ivWifi);
 
+        ConnectivityManager connMgr = (ConnectivityManager) getActivity()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null) {
+            isWifiConnected = networkInfo.isConnected() && networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
+        }
+        if (isWifiConnected) {
+            getWifiNetworkId();
+        }
 
+        ImageView ivSeeMe = (ImageView) rootView.findViewById(R.id.ivSeeMe);
+        ivSeeMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), ssid + ": " + networkId, Toast.LENGTH_SHORT).show();
+            }
+        });
         return rootView;
+    }
+
+    private void getWifiNetworkId() {
+        WifiManager wifiManager = (WifiManager) getActivity().getApplicationContext()
+                .getSystemService (Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        ssid  = wifiInfo.getSSID();
+        networkId = wifiInfo.getBSSID();
+        if (ssid.equals("<unknown ssid>")) {
+            Log.i("ActiveNetInfo", "Wifi Network Not Found: " + String.valueOf(ssid));
+            ivWifi.setImageResource(R.drawable.ic_wifi_off_white_48dp);
+        } else {
+            Log.i("ActiveNetInfo", "Wifi Network " + String.valueOf(ssid) + ": " + networkId);
+            ivWifi.setImageResource(R.drawable.ic_wifi_white_48dp);
+        }
     }
 
 }
