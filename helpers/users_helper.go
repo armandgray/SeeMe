3c25243b35a189e8806c7f2af5088809c99ecb52
Armand Gray <armandgray@gmail.com>
2017-03-17 21:37:38 -0700

    added for loop to reflect oldUser into newUser

--- helpers/users_helper.go
+++ helpers/users_helper.go
@@ -24,7 +24,7 @@ func CreateUserFromRequest(r *http.Request) (User) {
 
 func ReflectUsers(oldUser User, newUser User) (User) {
 	fmt.Println(oldUser)
-	fmt.Println(oldUser)
+	fmt.Println(newUser)
 	fmt.Println("\n\n\n")
 	oldUserAsFields := reflect.ValueOf(&oldUser).Elem()
   newUserAsFields := reflect.ValueOf(&newUser).Elem()
@@ -33,5 +33,21 @@ func ReflectUsers(oldUser User, newUser User) (User) {
   fmt.Println("newUser Fields: ", newUserAsFields)
   fmt.Println("typeOfUser: ", typeOfUser)
 
+  for i := 0; i < oldUserAsFields.NumField(); i++ {
+	fmt.Println("\n\n\n")
+    oldField := oldUserAsFields.Field(i)
+    newField:= newUserAsFields.Field(i)
+    fmt.Printf("%d: %s %s = %v\n", i,
+        typeOfUser.Field(i).Name, oldField.Type(), oldField.Interface())
+    fmt.Printf("%d: %s %s = %v\n", i,
+        typeOfUser.Field(i).Name, newField.Type(), newField.Interface())
+    newField.Set(reflect.Value(oldField))
+    fmt.Printf("%d: %s %s = %v\n", i,
+        typeOfUser.Field(i).Name, newField.Type(), newField.Interface())
+  }
+
+	fmt.Println("\n\n\n")
+  fmt.Println(oldUser)
+	fmt.Println(newUser)
 	return newUser
 }
