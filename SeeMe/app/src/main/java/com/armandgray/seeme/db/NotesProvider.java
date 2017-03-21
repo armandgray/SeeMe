6fdd31e76eb9411d9d7bf811e77b51540dfbe581
Armand Gray <armandgray@gmail.com>
2017-03-21 12:59:11 -0700

    added Code to CRUD interface methods in NotesProvider

--- SeeMe/app/src/main/java/com/armandgray/seeme/db/NotesProvider.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/db/NotesProvider.java
@@ -36,28 +36,31 @@ public class NotesProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        return database.query(DatabaseHelper.TABLE_NOTES, DatabaseHelper.ALL_COLUMNS,
+                selection, null, null, null,
+                DatabaseHelper.NOTE_CREATED + " DESC");
     }
 
     @Nullable
     @Override
-    public String getType(@NonNull Uri uri) {
-        return null;
-    }
+    public String getType(@NonNull Uri uri) { return null; }
 
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        long id = database.insert(DatabaseHelper.TABLE_NOTES, null, values);
+        return Uri.parse(BASE_PATH + "/" + id);
     }
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+        return database.delete(DatabaseHelper.TABLE_NOTES, selection, selectionArgs);
     }
 
     @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+    public int update(@NonNull Uri uri, @Nullable ContentValues values,
+                      @Nullable String selection, @Nullable String[] selectionArgs) {
+        return database.update(DatabaseHelper.TABLE_NOTES,
+                values, selection, selectionArgs);
     }
 }
