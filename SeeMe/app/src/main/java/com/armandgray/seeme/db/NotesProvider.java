077243b74e982e010b3c856fb07ed0d0e58659d2
Armand Gray <armandgray@gmail.com>
2017-03-21 12:43:37 -0700

    added NotesProvider extends ContentProvider

---
+++ SeeMe/app/src/main/java/com/armandgray/seeme/db/NotesProvider.java
@@ -0,0 +1,52 @@
+package com.armandgray.seeme.db;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class NotesProvider extends ContentProvider {
+
+    private static final String AUTHORITY = "com.armandgray.seeme.db.notesprovider";
+    private static final String BASE_PATH = "notes";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+    private static final int NOTES = 1;
+    private static final int NOTES_ID = 2;
+
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
