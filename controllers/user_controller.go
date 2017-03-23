b46f571d63d3988da14f6d3230642d9961f21765
Armand Gray <armandgray@gmail.com>
2017-03-23 13:23:35 -0700

    refactored all network related functions into network_controller.go

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -5,7 +5,6 @@ import (
   . "seeme/helpers"
 
   "net/http"
-  "errors"
 
 	"database/sql"
   _ "github.com/go-sql-driver/mysql"
@@ -66,41 +65,6 @@ func UpdateUser(user User) (error) {
   _, err := db.Exec("UPDATE users SET first_name=?, last_name=?, role=?, secret=?, discoverable=? WHERE username = ?", 
                   user.FirstName, user.LastName, user.Role, 
                   user.Secret, user.Discoverable, user.Username)
-  return err
-}
-
-func RenewUserNetwork(r *http.Request) (error) {
-	db := GetDatabaseInstance()
-  var networkId string
-  row := db.QueryRow("select network_id from networks where network_id = ?", r.FormValue("networkId"))
-  if err := row.Scan(&networkId); err != nil {
-    return errors.New("Internal Network Error!")
-  }
-
-  if networkId == "" {
-    if err := insertNewNetwork(r); err != nil {
-    return errors.New("Network ID Error!")
-    }
-  }
-
-  if err := updateUserNetwork(r); err != nil {
-    return errors.New("Network Update Error!")
-  }
-  
-  return nil
-}
-
-func insertNewNetwork(r *http.Request) (error) {
-	db := GetDatabaseInstance()
-  _, err := db.Exec("INSERT INTO networks (network_id, ssid) VALUES (?, ?)", 
-                  r.FormValue("networkId"), r.FormValue("ssid"))
-  return err
-}
-
-func updateUserNetwork(r *http.Request) (error) {
-	db := GetDatabaseInstance()
-  _, err := db.Exec("UPDATE users SET network_id=? WHERE username= ?", 
-                  r.FormValue("networkId"), r.FormValue("username"))
   return err
 }
 
