575c0ab667ca31fb8a6b5c21fa179ebe74cfb8a4
Armand Gray <armandgray@gmail.com>
2017-03-23 12:50:45 -0700

    removed null from receiver

--- SeeMe/app/src/main/java/com/armandgray/seeme/views/DiscoverFragment.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/views/DiscoverFragment.java
@@ -55,7 +55,6 @@ public class DiscoverFragment extends Fragment {
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.e("BroadcastReceiver: ", "http Broadcast Received");
-            userArray = null;
             userArray = (User[]) intent.getParcelableArrayExtra(HttpService.HTTP_SERVICE_JSON_PAYLOAD);
             if (userArray != null && userArray.length != 0) {
                 setupRvUsers(Arrays.asList(userArray));
@@ -162,7 +161,6 @@ public class DiscoverFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-
         LocalBroadcastManager.getInstance(getActivity().getApplicationContext())
                 .registerReceiver(httpBroadcastReceiver,
                         new IntentFilter(HttpService.HTTP_SERVICE_MESSAGE));
