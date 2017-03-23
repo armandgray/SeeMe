b2f1af80f96a89c6a6eab0e24b1be5ebc4e4e524
Armand Gray <armandgray@gmail.com>
2017-03-23 13:42:22 -0700

    added validation for passed arguments and db validation for user existence

--- routes/connection_routes.go
+++ routes/connection_routes.go
@@ -7,9 +7,21 @@ import (
 )
 
 func HandlerNewConnection(w http.ResponseWriter, r *http.Request) {
-  _, err := GetUserFromDB(r.FormValue("username"))
-  if err != nil {
+  username := r.FormValue("username")
+  connection := r.FormValue("connection")
+  if username == "" || connection == "" || username == connection {
+    w.Write([]byte("Invalid Connection!"))
+    return
+  }
+  if _, err := GetUserFromDB(username); err != nil {
     w.Write([]byte("User Not Found!"))
     return
-  }    
+  }
+
+  if _, err := GetUserFromDB(connection); err != nil {
+    w.Write([]byte("Requested User Not Found!"))
+    return
+  }
+    
+  w.Write([]byte("Request Sent"))
 }
