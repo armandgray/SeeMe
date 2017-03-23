e5d80254a459413ea072e6e76927ea171b76f653
Armand Gray <armandgray@gmail.com>
2017-03-23 13:21:48 -0700

    refactored GetDiscoverableUsersFromDB & GetLocalUsersForNetwork into discoverable_controller.go

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -69,39 +69,6 @@ func UpdateUser(user User) (error) {
   return err
 }
 
-func GetDiscoverableUsersFromDB(w http.ResponseWriter) ([]User) {
-	db := GetDatabaseInstance()
-  var userList []User
-  var user User
-
-  rows, err := db.Query("select * from users where discoverable = 1 AND !(network_id = 'NULL')")
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
-  defer rows.Close()
-  for rows.Next() {
-    if err = rows.Scan(&user.FirstName, &user.LastName, &user.Role, &user.Username, 
-                        &user.Secret, &user.Discoverable, &user.Network); err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-    } else {
-      userList = append(userList, user)
-    }
-  }
-  if err = rows.Err(); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-  }
-
-  return userList
-}
-
-func GetLocalUsersForNetwork(w http.ResponseWriter, r *http.Request) ([]User, error) {
-  if err := RenewUserNetwork(r); err != nil {
-    var userList []User
-    return userList, err
-  }
-  return getExistingUsersForNetwork(w, r), nil
-}
-
 func RenewUserNetwork(r *http.Request) (error) {
 	db := GetDatabaseInstance()
   var networkId string
