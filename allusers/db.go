99cfe99d49a28dfb69d2546c337ac9abc723b071
Armand Gray <armandgray@gmail.com>
2017-03-24 13:47:07 -0700

    refactored alluser code into allusers/

--- allusers/db.go
+++ allusers/db.go
@@ -1,14 +1,12 @@
-package controllers
+package allusers
 
 import (
-	"seeme/db"
-
   "net/http"
   "encoding/json"
 )
 
 func AllUsersController(w http.ResponseWriter, r *http.Request) {
-  js, err := json.Marshal(db.GetDiscoverableUsers(w))
+  js, err := json.Marshal(GetDiscoverableUsers(w))
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
