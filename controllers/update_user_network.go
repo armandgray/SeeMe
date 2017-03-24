bcfe903aac22c779c3913491cece331d516504bc
Armand Gray <armandgray@gmail.com>
2017-03-24 11:33:33 -0700

    removed network_controller

--- controllers/update_user_network.go
+++ controllers/update_user_network.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
+	"seeme/db"
+
   "net/http"
   "fmt"
 )
 
 func UpdateUserNetworkController(w http.ResponseWriter, r *http.Request) {
-  if err := RenewUserNetwork(r); err != nil {
+  if err := db.RenewUserNetwork(r); err != nil {
     fmt.Println("Network Update Error")
   }
 }
