808f6c0d1d230770886b7bfe4d72014e9079a71a
Armand Gray <armandgray@gmail.com>
2017-03-26 20:33:41 -0700

    removed all template code in registeruser

--- registeruser/controller.go
+++ registeruser/controller.go
@@ -1,29 +1,20 @@
 package registeruser
 
 import (
-  . "seeme/models"
-  . "seeme/helpers"
+  "seeme/helpers"
   "seeme/db"
 
   "fmt"
   "net/http"
-  "html/template"
 )
 
 func RegisterUserController(w http.ResponseWriter, r *http.Request) {
-  templLogin := template.Must(template.ParseFiles("views/register.html"))
-  var page Page
-
   if r.FormValue("register") != "" {
-    if err := db.InsertUser(CreateUserFromRequest(r)); err != nil { 
+    if err := db.InsertUser(helpers.CreateUserFromRequest(r)); err != nil { 
       fmt.Println("Database Insert Failure: " + err.Error())
     } else {
       http.Redirect(w, r, "/seeme/api/login/user?username=" + r.FormValue("username"), http.StatusFound)
       return
     }
-  }
-
-  if err := templLogin.ExecuteTemplate(w, "register.html", page); err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
   }
 }
