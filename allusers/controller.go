09f28d013f5e56036baa1953ec71c61d04a886e1
Armand Gray <armandgray@gmail.com>
2017-03-28 12:54:00 -0700

    refactorec allusers/helper --> db_instance.GetQueryUserList

--- allusers/controller.go
+++ allusers/controller.go
@@ -1,12 +1,22 @@
 package allusers
 
 import (
+	"seeme/db"
+
+	"fmt"
   "net/http"
   "encoding/json"
 )
 
 func AllUsersController(w http.ResponseWriter, r *http.Request) {
-  js, err := json.Marshal(GetDiscoverableUsers(w))
+	userList, err := db.GetQueryUserList(r.FormValue("username"))
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+	}
+
+  js, err := json.Marshal(userList)
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
