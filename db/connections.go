2ceec4c4db2d282d1a6e82320d31e90fae0dac7c
Armand Gray <armandgray@gmail.com>
2017-03-27 18:17:17 -0700

    added UpdateConnectionStatus

--- db/connections.go
+++ db/connections.go
@@ -5,3 +5,10 @@ func InsertNewConnection(username string, connection string) (error) {
 	_, err := db.Exec("INSERT INTO connections VALUES (?, ?)", username, connection)
 	return err
 }
+
+func UpdateConnectionStatus(username string, connection string, status string) (error) {
+	db := GetDatabaseInstance()
+	_, err := db.Exec("UPDATE connections SET status = ? WHERE username = ? AND connection = ?",
+												status, username, connection)
+	return err
+}
