2ba62e253c95ed74b7cf12fcb2cc21a286b71dbc
Armand Gray <armandgray@gmail.com>
2017-03-29 16:24:20 -0700

    correctly implemented parcelable in User.class

--- SeeMe/app/src/main/java/com/armandgray/seeme/models/User.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/models/User.java
@@ -57,6 +57,8 @@ public class User implements Parcelable {
         return status;
     }
 
+
+
     @Override
     public int describeContents() {
         return 0;
@@ -64,6 +66,36 @@ public class User implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-
+        dest.writeString(this.occupation);
+        dest.writeString(this.firstName);
+        dest.writeString(this.lastName);
+        dest.writeString(this.username);
+        dest.writeString(this.secret);
+        dest.writeByte(this.discoverable ? (byte) 1 : (byte) 0);
+        dest.writeString(this.network);
+        dest.writeString(this.status);
     }
+
+    protected User(Parcel in) {
+        this.occupation = in.readString();
+        this.firstName = in.readString();
+        this.lastName = in.readString();
+        this.username = in.readString();
+        this.secret = in.readString();
+        this.discoverable = in.readByte() != 0;
+        this.network = in.readString();
+        this.status = in.readString();
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
