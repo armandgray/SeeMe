a322194d7ffa43ba0a53b5785082310d6b4ba60a
Armand Gray <armandgray@gmail.com>
2017-03-28 22:27:45 -0700

    added responseArray flag and toast message for httpResponse

--- SeeMe/app/src/main/java/com/armandgray/seeme/controllers/NetworkFragmentController.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/controllers/NetworkFragmentController.java
@@ -7,15 +7,23 @@ import android.widget.Toast;
 import com.armandgray.seeme.models.User;
 import com.armandgray.seeme.views.NetworkFragment;
 
+import java.util.Arrays;
+
 import static com.armandgray.seeme.network.HttpHelper.sendRequest;
 import static com.armandgray.seeme.views.NetworkFragment.NETWORK_CONNECTION_URI;
 
-/**
- * Created by armandgray on 3/28/17.
- */
-
 public class NetworkFragmentController implements NetworkFragment.NetworkController {
 
+    private static final String USER_NOT_FOUND = "User Not Found!";
+    private static final String CONNECTION_CONFIRMED = "Connection Confirmed";
+    private static final String CONNECTION_DELETED = "Connection Deleted!";
+    private static final String REQUEST_SENT = "Request Sent";
+    private static final String PREPARE_UPDATE_ERROR = "Prepare Update Error!";
+    private static final String UPDATE_QUERY_ERROR = "Update Query Error!";
+    private static final String INTERNAL_UPDATE_ERROR = "Internal Update Error!";
+
+    private String[] responseArray = {USER_NOT_FOUND, PREPARE_UPDATE_ERROR, CONNECTION_CONFIRMED, CONNECTION_DELETED,
+            REQUEST_SENT, UPDATE_QUERY_ERROR, INTERNAL_UPDATE_ERROR};
     private Context context;
     private User activeUser;
 
@@ -32,6 +40,8 @@ public class NetworkFragmentController implements NetworkFragment.NetworkControl
 
     @Override
     public void handleHttpResponse(String response, Parcelable[] arrayExtra) {
-        Toast.makeText(context, "Response Received", Toast.LENGTH_SHORT).show();
+        if (response != null && !response.equals("") && Arrays.asList(responseArray).contains(response)) {
+            Toast.makeText(context, response, Toast.LENGTH_SHORT).show();
+        }
     }
 }
