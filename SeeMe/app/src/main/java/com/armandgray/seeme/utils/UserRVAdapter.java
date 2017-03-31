d1da62ddcbbede115d077ddf82b37147bf8476ce
Armand Gray <armandgray@gmail.com>
2017-03-30 19:35:25 -0700

    added index flags for each status; added corresponding code to getItemViewType

--- SeeMe/app/src/main/java/com/armandgray/seeme/utils/UserRVAdapter.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/utils/UserRVAdapter.java
@@ -11,49 +11,63 @@ import android.widget.TextView;
 import com.armandgray.seeme.R;
 import com.armandgray.seeme.models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRVAdapter extends
-        RecyclerView.Adapter<UserRVAdapter.ViewHolder> {
+        RecyclerView.Adapter<UserRVAdapter.ItemViewHolder> {
 
-    private static final int TYPE_REQUEST = 0;
-    private static final int TYPE_PENDING = 1;
-    private static final int TYPE_CONNECTED = 2;
+    private static final int TYPE_ITEM = 0;
+    private static final int TYPE_REQUEST_HEADER = 1;
+    private static final int TYPE_PENDING_HEADER = 2;
+    private static final int TYPE_CONNECTED_HEADER = 3;
 
     private static final String UNKNOWN = "unknown";
     private static final String CONNECTED = "connected";
     private static final String PENDING = "pending";
     private static final String REQUEST = "request";
     private static final String TAG = "USER_RV_ADAPTER";
-
     private List<User> listUsers;
-    private boolean showRequest;
-    private String[] statusArray;
 
+    private boolean showRequest;
+    private List<String> statusList;
+    private final boolean isfirstUserRequest;
+    private final int firstPendingIndex;
+    private final int firstConnectedIndex;
 
     public UserRVAdapter(List<User> listUsers, boolean showRequest) {
         this.listUsers = listUsers;
         this.showRequest = showRequest;
-        this.statusArray = new String[listUsers.size()];
+        this.statusList = new ArrayList<>();
         for (int i = 0; i < listUsers.size(); i++) {
-            statusArray[i] = listUsers.get(i).getStatus();
-            Log.i(TAG, statusArray[i]);
+            statusList.add(listUsers.get(i).getStatus());
+            Log.i(TAG, statusList.get(i));
         }
+        isfirstUserRequest = statusList.indexOf("request") == 0;
+        firstPendingIndex = statusList.indexOf("pending");
+        firstConnectedIndex = statusList.indexOf("connected");
     }
 
     @Override
     public int getItemViewType(int position) {
-        return super.getItemViewType(position);
+        if (isfirstUserRequest && position == 0) {
+            return TYPE_REQUEST_HEADER;
+        } else if (position == firstPendingIndex) {
+            return TYPE_PENDING_HEADER;
+        } else if (position == firstConnectedIndex) {
+            return TYPE_CONNECTED_HEADER;
+        }
+        return TYPE_ITEM;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.user_listitem, parent, false));
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new ItemViewHolder(LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.user_listitem, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(UserRVAdapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(ItemViewHolder viewHolder, int position) {
         User user = listUsers.get(position);
 
         ImageView ivUserProfile = viewHolder.ivUserProfile;
@@ -88,13 +102,13 @@ public class UserRVAdapter extends
         return listUsers.size();
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
         ImageView ivUserProfile;
         TextView tvUserName;
         TextView tvRole;
         ImageView ivStatus;
 
-        ViewHolder(View itemView) {
+        ItemViewHolder(View itemView) {
             super(itemView);
             ivUserProfile = (ImageView) itemView.findViewById(R.id.ivUserProfile);
             tvUserName = (TextView) itemView.findViewById(R.id.tvFullName);
