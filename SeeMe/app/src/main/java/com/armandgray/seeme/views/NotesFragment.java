0564c370209a339daf9853765d3e9d825cf3948d
Armand Gray <armandgray@gmail.com>
2017-04-02 19:35:28 -0700

    added broadcast receiver and reg/unreg

--- SeeMe/app/src/main/java/com/armandgray/seeme/views/NotesFragment.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/views/NotesFragment.java
@@ -1,16 +1,21 @@
 package com.armandgray.seeme.views;
 
 
+import android.content.BroadcastReceiver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.CursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,11 +24,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.armandgray.seeme.MainActivity;
 import com.armandgray.seeme.NoteEditorActivity;
 import com.armandgray.seeme.R;
 import com.armandgray.seeme.db.DatabaseHelper;
 import com.armandgray.seeme.db.NotesProvider;
 import com.armandgray.seeme.models.User;
+import com.armandgray.seeme.services.HttpService;
 import com.armandgray.seeme.utils.NotesLvAdapter;
 
 import org.json.JSONArray;
@@ -47,6 +54,17 @@ public class NotesFragment extends Fragment
     private static final int EDITOR_REQUEST_CODE = 1001;
     private CursorAdapter adapter;
     private User activeUser;
+
+    private BroadcastReceiver httpBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "http Broadcast Received");
+            Parcelable[] arrayExtra = intent.getParcelableArrayExtra(HttpService.HTTP_SERVICE_JSON_PAYLOAD);
+            notesArray = (User[]) arrayExtra;
+            handleHttpResponse(
+                    intent.getStringExtra(HttpService.HTTP_SERVICE_STRING_PAYLOAD), arrayExtra);
+        }
+    };
 
     public NotesFragment() {}
 
@@ -166,6 +184,25 @@ public class NotesFragment extends Fragment
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == EDITOR_REQUEST_CODE && resultCode == RESULT_OK) {
             restartLoader();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getUserVisibleHint()) {
+            LocalBroadcastManager.getInstance(getActivity().getApplicationContext())
+                    .registerReceiver(httpBroadcastReceiver,
+                            new IntentFilter(HttpService.HTTP_SERVICE_MESSAGE));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (!getUserVisibleHint()) {
+            LocalBroadcastManager.getInstance(getActivity().getApplicationContext())
+                    .unregisterReceiver(httpBroadcastReceiver);
         }
     }
 
