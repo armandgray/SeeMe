706c466b9bf79cca81d3ed6e4e5e6c61ca79ae93
Armand Gray <armandgray@gmail.com>
2017-04-07 09:54:09 -0700

    added calls to intent.getAction() in WifiReceiver

--- SeeMe/app/src/main/java/com/armandgray/seeme/network/WifiReceiver.java
+++ SeeMe/app/src/main/java/com/armandgray/seeme/network/WifiReceiver.java
@@ -5,16 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 
 import com.armandgray.seeme.observers.BroadcastObserver;
 
 public class WifiReceiver extends BroadcastReceiver {
+
+    public static final String TAG = "WifiReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        ConnectivityManager connectivityManager =
-                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
-        BroadcastObserver.getInstance().updateWifiState(activeNetInfo);
+        if (intent.getAction().equals(WifiManager.WIFI_STATE_CHANGED_ACTION)
+                || intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            ConnectivityManager connectivityManager =
+                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+            BroadcastObserver.getInstance().updateWifiState(activeNetInfo);
+        }
     }
 
 }
